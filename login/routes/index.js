573d9ffcc7eb20c75f0a620b0cd03953ebcb1f01
jxh <jxh@jxhs-MBP.lan>
2018-06-01 16:00:39 +0800

    socketIO集成

--- login/routes/index.js
+++ login/routes/index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, response, next) {
-  // res.render('index', { title: 'geme' });
-  response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});   
-        str="<h1>欢迎</h1><a href='/login'>点我登录</a>";  
-         response.write(str);   
-         response.end();   
-});
+// router.get('/', function(req, response, next) {
+//   // res.render('index', { title: 'geme' });
+//   response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});   
+//         str="<h1>欢迎</h1><a href='/login'>点我登录</a>";  
+//          response.write(str);   
+//          response.end();   
+// });
 
+
+router.get('/', function(req, response, next) {
+  response.sendFile(__dirname + '/../views/chat.html');
+});
 module.exports = router;
