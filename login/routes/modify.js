522a69761eee8a75254528a9fb9577f9eac324c1
hufans <434880565@qq.com>
2018-06-11 18:17:36 +0800

    代码修改

--- login/routes/modify.js
+++ login/routes/modify.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var mysql = require('./../mysql/sql'); 
 
 router.get('/', function(req,res,data){
-
     var sql = "SELECT * FROM t_user WHERE id = '"+req.session.user.id+"';";  
     console.log(sql)
     mysql.query(sql,function(err, dataR){  
@@ -11,15 +10,38 @@ router.get('/', function(req,res,data){
         modifyview(req,res,dataR);  
     }) 
 });
+
+router.post("/msg",function(req,res,next){
+    req.setEncoding("utf8"); 
+    var params = req.body
+    var name = params.name;
+    modifyMesg(req,res,next)
+})
+
+router.post("/header",function(req,res,next){
+    router.form = new formidable.IncomingForm();
+    router.form.parse(req, function(err, fields, files) {
+        if (!err){
+            //modify user header
+            modifyUserHeader(req,res,fields,files);
+        }else{
+            next();
+        }
+    });
+})
+
+
+
+
 function modifyview(req,res,data){ 
 
-    req.session.userData = {};
-    req.session.userData.login = true;
-    req.session.userData.userName = data[0].name;
-    req.session.userData.tel = data[0].tel;
-    req.session.userData.age = data[0].age;
-    req.session.userData.id = data[0].id;
-
+    req.session.userData = {
+        login:true,
+        userName = data[0].name,
+        tel = data[0].name,
+        age = data[0].name,
+        id = data[0].name
+    };
     res.writeHead(200,{'Content-type':'text/html;charset=utf-8'}); 
     //upload header img 
     var str = "<img src=\"/static/images/header.png\"alt=\"header\" />";
